75ce3887927fe7c1684197fe54247083976d394f
JuanBisnetoc <juanmiquelsalabisnetu2@gmail.com>
2026-04-21 11:51:48 -0300

    Add files via upload

---
+++ 1005.py
@@ -0,0 +1,8 @@
+A= float(input())*3.5
+B= float(input())*7.5
+#media1= A*3.5
+#media2= B*7.5
+#media3= media1+media2
+media=A+B
+media_final=media/11
+print(f'MEDIA = {media_final:.5f}')
---
+++ 1006.py
@@ -0,0 +1,5 @@
+A= float(input())*2.0
+B= float(input())*3.0
+C= float(input())*5.0
+media= (A+B+C)/10
+print(f'MEDIA= {media:.1f}')
